bd437a85cda8f36ad156804150f3b4a3295967bb
bryangwhiting <bryan.g.whiting@gmail.com>
2020-02-02 11:14:43 -0500

    Added renv

---
+++ packages.R
@@ -0,0 +1,23 @@
+# install.load::install_load('dplyr', 'htmltools', 'knitr', 'tidyr', 'ggplot2', 'kable', 'kableExtra', 'shiny', 'magrittr', 'lubridate', 'formattable', 'ggthemes')
+
+library(readr)
+library(distill)
+library(dplyr)
+library(htmltools)
+library(knitr)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+library(kableExtra)
+# library(shiny)
+library(magrittr)
+library(lubridate)
+library(formattable)
+library(ggthemes)
+library(DT)
+
+# Save the environment
+# renv::snapshot()
+
+# leaflet
+# 
