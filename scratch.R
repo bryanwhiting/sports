75be327801bdd3a01e59bebc77c746f099abf200
bryangwhiting <bryan.g.whiting@gmail.com>
2020-02-08 16:52:29 -0500

    Daily batch run

--- scratch.R
+++ scratch.R
@@ -98,3 +98,44 @@ df_today %>%
 #   ) #%>%
 # kable(escape=F, digits=3) %>%
 # kable_styling(c('striped', 'hover'), full_width = F)
+
+
+
+
+# CALCULATE RECORDS -----
+mod_accs = c()
+df_proj = df
+models = c('raptor', 'v02', 'carm-elo', 'elo')
+for (mod in models){
+  res_mod = paste0(mod, '_result')
+  pred = as.numeric(df[[mod]] > 0.5)
+  df_proj[[res_mod]] = ifelse(is.na(df$result), pred, df$result)
+}
+df_proj$elo_result == df_proj$result
+
+# assert predictions are the same
+# https://projects.fivethirtyeight.com/2020-nba-predictions/
+res_name = 'elo_result'
+res_name = 'v02_result'
+res_name = 'raptor_result'
+res_name = 'carm-elo_result'
+res_name = 'result'
+df_proj %>%
+  filter(!is.na(get(res_name))) %>%
+  mutate(
+    result1 = get(res_name),
+    result2 = ifelse(get(res_name) == 1, 0, 1)
+  ) %>%
+  select(team1, team2, result1, result2) %>%
+  # reshape long to be team | result
+  gather('team', 'result', -team1, -team2) %>%
+  mutate(team = ifelse(team == 'result1', team1, team2)) %>%
+  select(-team1, -team2) %>%
+  # Count wins and losses
+  group_by(team) %>%
+  summarize(wins = sum(result == 1),
+            losses = sum(result == 0),
+            n_games = wins + losses) %>%
+  mutate(pct = round(wins/(wins + losses), 2) * 100) %>%
+  arrange(desc(pct))
+
